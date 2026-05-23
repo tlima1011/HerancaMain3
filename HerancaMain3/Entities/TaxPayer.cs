eb2543c526e325ae4e1d750f614aecddf9e3cdb0
tlima1011 <thiagolima1011@gmail.com>
2026-05-23 14:29:29 -0300

    Class TaxPayer Created

---
+++ HerancaMain3/Entities/TaxPayer.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Globalization; 
+
+namespace HerancaMain3.Entities
+{
+    internal abstract class TaxPayer
+    {
+        protected string Name { get; set; }
+        public double AnualIncome { get; set; }
+
+        public TaxPayer(string name, double anualIncome)
+        {
+            Name = name;
+            AnualIncome = anualIncome;
+        }
+
+        public abstract double Tax();
+
+        public override string ToString()
+        {
+            return Name + ": $ " + Tax().ToString("F2", CultureInfo.InvariantCulture);
+        }
+    }
+}

d306d502759cd5a24287b68ced55a7ea16f4d7f7
tlima1011 <thiagolima1011@gmail.com>
2026-05-23 14:23:26 -0300

    Projeto Criado

---
+++ HerancaMain3/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace HerancaMain3
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Projeto Criado Herança Main 3 - C#");
+        }
+    }
+}

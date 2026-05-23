951bdab3e5ce802e8e6940f91780517c69ccb609
tlima1011 <thiagolima1011@gmail.com>
2026-05-23 14:39:51 -0300

    Class Company Created

---
+++ HerancaMain3/Entities/Company.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace HerancaMain3.Entities
+{
+    internal class Company : TaxPayer
+    {
+
+        public int NumberOfEmployees { get; set; }
+        public Company(string name, double anualIncome, int numberOfEmployees) : base(name, anualIncome)
+        {
+            NumberOfEmployees = numberOfEmployees; 
+        }
+
+        public override double Tax()
+        {
+           return (NumberOfEmployees < 10) ? (AnualIncome * 0.16) : (AnualIncome * 0.14);
+        }
+    }
+}

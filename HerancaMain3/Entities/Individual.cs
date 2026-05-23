63b37337ed9fdd39baa8907e4db630cd828c6682
tlima1011 <thiagolima1011@gmail.com>
2026-05-23 14:35:37 -0300

    Class Individual Created

---
+++ HerancaMain3/Entities/Individual.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace HerancaMain3.Entities
+{
+    internal class Individual : TaxPayer
+    {
+
+        public double HealthExpenditures { get; set; }
+        public Individual(string name, double anualIncome, double healthExpenditures) : base(name, anualIncome)
+        {
+            HealthExpenditures = healthExpenditures; 
+        }
+
+        public override double Tax()
+        {
+            return (AnualIncome < 20000.0) ? (AnualIncome * 0.15) - (HealthExpenditures * 0.5) : (AnualIncome * 0.25) - (HealthExpenditures * 0.5);
+        }
+    }
+}
